549f99d201df48fe7c3b661b7857c6fb92c32132
aatack <alexanderjtatack@gmail.com>
2021-07-02 22:55:54 +0100

    Set up a basic window

---
+++ graphics/window.py
@@ -0,0 +1,28 @@
+import pygame
+from typing import Tuple
+
+
+class Window:
+    def __init__(
+        self,
+        width: int = 500,
+        height: int = 500,
+        background_colour: Tuple[int, int, int] = (255, 255, 255),
+    ):
+        pygame.init()
+
+        self.surface = pygame.display.set_mode((width, height), pygame.RESIZABLE)
+        self.background_colour = background_colour
+
+    def loop(self) -> bool:
+        for event in pygame.event.get():
+            if event.type == pygame.VIDEORESIZE:
+                self.surface = pygame.display.set_mode(
+                    (event.w, event.h), pygame.RESIZABLE
+                )
+            if event.type == pygame.QUIT:
+                return False
+
+        self.surface.fill(self.background_colour)
+
+        return True
