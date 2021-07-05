4e32fde21b027569a46e290d4553b33b0e3e12ea
aatack <alexanderjtatack@gmail.com>
2021-07-05 21:21:29 +0100

    Break renderables down into more atomic parts

--- graphics/renderables.py
+++ graphics/renderables.py
@@ -3,21 +3,38 @@ from graphics.colours import Colour
 import pygame
 
 
-class Group(NamedTuple):
+Renderable = NamedTuple
+
+
+class Group(Renderable):
     children: tuple
 
     def render(self, surface: pygame.Surface):
         for child in self.children:
             child.render(surface)
 
+    def translate(self, x: int = 0, y: int = 0) -> Renderable:
+        return Group(tuple(child.translate(x, y) for child in self.children))
 
-class Circle(NamedTuple):
-    horizontal: int
-    vertical: int
+
+class Point(Renderable):
+    x: int
+    y: int
+
+    def render(self, surface: pygame.Surface):
+        pass
+
+    def translate(self, x: int = 0, y: int = 0) -> Renderable:
+        return Point(self.x + x, self.y + y)
+
+
+class Circle(Renderable):
+    origin: Point
     radius: int
     colour: Colour
 
     def render(self, surface: pygame.Surface):
-        pygame.draw.circle(
-            surface, self.colour, (self.horizontal, self.vertical), self.radius
-        )
+        pygame.draw.circle(surface, self.colour, self.origin, self.radius)
+
+    def translate(self, x: int = 0, y: int = 0) -> Renderable:
+        return Circle(self.origin.translate(x, y), self.radius, self.colour)
