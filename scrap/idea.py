e54113d01122dd38f3513bedb2d99dc85852d9d5
aatack <alexanderjtatack@gmail.com>
2021-07-06 22:09:06 +0100

    Reverse the direction of multiple dispatch

--- scrap/idea.py
+++ scrap/idea.py
@@ -1,4 +1,6 @@
 from typing import Callable, Dict, Type
+from collections import namedtuple
+import inspect
 
 
 class ScrapDispatch:
@@ -18,17 +20,46 @@ class ScrapDispatch:
         cls.dispatch(actor)[actee] = function
 
 
-def scrap(name: str) -> Callable:
+def scrap(actee: str) -> Callable:
     def decorator(constructor: Type) -> Type:
-        return constructor
+        definition = {k: v for k, v in inspect.getmembers(constructor)}
+
+        fields = []
+        defaults = []
+
+        for field_name, field_type in definition["__annotations__"].items():
+            fields.append(field_name)
+            if len(defaults) > 0:
+                assert field_name in definition
+            if field_name in definition:
+                defaults.append(definition[field_name])
+
+        for actor, function in definition.items():
+            if not actor.startswith("__") and actor not in fields:
+                assert type(function).__name__ == "function"
+                ScrapDispatch.register(actor, actee, function)
+
+        modified_constructor = namedtuple(actee, fields, defaults=defaults)
+
+        dispatch_dictionary = ScrapDispatch.dispatch(actee)
+
+        def __call__(_actor, _actee):
+            function = dispatch_dictionary.get(type(_actee).__name__, None)
+            if function is not None:
+                return function(_actee, _actor)
+            else:
+                return _actee
+
+        setattr(modified_constructor, "__call__", __call__)
+        return modified_constructor
 
     return decorator
 
 
 @scrap("translate")
 class Translate:
-    right: float
-    down: float
+    right: float = 0.0
+    down: float = 0.0
 
 
 @scrap("point")
