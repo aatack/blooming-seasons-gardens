f88d9923cdb90417fd801563860f5d1100d9722d
aatack <alexanderjtatack@gmail.com>
2021-08-15 00:38:49 +0100

    Update data and events

--- scrap/data.py
+++ scrap/data.py
@@ -4,66 +4,31 @@ from typing import List, Any
 import pygame
 
 
-class Literal(Scrap):
-    def __init__(self, value: Any):
-        self.value = value
-
-    def __str__(self) -> str:
-        return f"`{self.value}"
+@defscrap
+class Literal:
+    value: Any
 
 
-class Message(Scrap):
-    def __init__(self, scrap: Scrap, message: Scrap):
-        self.scrap = scrap
-        self.message = message
-
-        assert not isinstance(self.scrap, Message), "Nested message inside scrap"
-        assert not isinstance(self.message, Message), "Nested message inside message"
-
-    @staticmethod
-    def collapse(scrap: Scrap, message: "Message") -> "Message":
-        return Message(
-            scrap, message.message if isinstance(message, Message) else message
-        )
-
-    def __str__(self) -> str:
-        return f"Message(scrap={self.scrap}, message={self.message})"
+@defscrap
+class Message:
+    # TODO: validation
+    scrap: Scrap
+    message: Scrap
 
 
-class Point(Scrap):
-    def __init__(self, x: float = 0.0, y: float = 0.0):
-        self.x = x
-        self.y = y
-
-    def cache(self) -> renderable.Renderable:
-        return renderable.Point(int(self.x), int(self.y))
-
-    def left(self, change: float) -> "Point":
-        return Point(self.x - change, self.y)
-
-    def right(self, change: float) -> "Point":
-        return Point(self.x + change, self.y)
-
-    def up(self, change: float) -> "Point":
-        return Point(self.x, self.y - change)
-
-    def down(self, change: float) -> "Point":
-        return Point(self.x, self.y + change)
-
-    def __str__(self) -> str:
-        return f"Point(x={self.x}, y={self.y})"
+@defscrap
+class Point:
+    x: float = 0.0
+    y: float = 0.0
 
 
-class Colour(Scrap):
-    def __init__(self, red: float = 0.0, green: float = 0.0, blue: float = 0.0):
-        self.red = red
-        self.green = green
-        self.blue = blue
+@defscrap
+class Colour:
+    red: float = 0.0
+    green: float = 0.0
+    blue: float = 0.0
 
-    def cache(self) -> renderable.Renderable:
+    def _cache(self) -> renderable.Renderable:
         return renderable.Colour(
             int(self.red * 255), int(self.green * 255), int(self.blue * 255)
         )
-
-    def __str__(self) -> str:
-        return f"Colour(red={self.red}, green={self.green}, blue={self.blue})"
--- scrap/event.py
+++ scrap/event.py
@@ -1,54 +1,34 @@
-from scrap.base import Scrap
+from scrap.base import defscrap
 from scrap.data import Point
 
 
-class Key(Scrap):
-    def __init__(self, key: int, down: bool = False, up: bool = False):
-        self.key = key
-        self.down = down
-        self.up = up
-
-        assert self.down is (not self.up)
-
-    def __str__(self) -> str:
-        return f"Key(key={self.key}, down={self.down}, up={self.up})"
+@defscrap
+class Key:
+    # TODO: validation
+    key: int
+    down: bool = False
+    up: bool = False
 
 
-class Button(Scrap):
-    def __init__(
-        self, button: int, location: Point, down: bool = False, up: bool = False
-    ):
-        self.button = button
-        self.location = location
-        self.down = down
-        self.up = up
-
-        assert self.down is (not self.up)
-
-    def __str__(self) -> str:
-        return (
-            f"Button(button={self.button}, location={self.location}, "
-            f"up={self.up}, down={self.down})"
-        )
+@defscrap
+class Button:
+    # TODO: validation
+    button: int
+    location: Point
+    down: bool = False
+    up: bool = False
 
 
-class Movement(Scrap):
-    def __init__(self, beginning: Point, end: Point):
-        self.beginning = beginning
-        self.end = end
+@defscrap
+class Movement:
+    start: Point
+    end: Point
 
-    @property
     def displacement(self) -> Point:
-        return Point(self.end.x - self.beginning.x, self.end.y - self.beginning.y)
-
-    def __str__(self) -> str:
-        return f"Movement(beginning={self.beginning}, end={self.end})"
+        return Point(self.end.x - self.start.x, self.end.y - self.start.y)
 
 
-class Click(Scrap):
-    def __init__(self, button: int, location: Point):
-        self.button = button
-        self.location = location
-
-    def __str__(self) -> str:
-        return f"Click(button={self.button}, location={self.location})"
+@defscrap
+class Click:
+    button: int
+    location: Point
