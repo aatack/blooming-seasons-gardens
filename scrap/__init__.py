fdb181d93c31082e2917c5336b7cd144c061ebcb
aatack <alexanderjtatack@gmail.com>
2021-08-15 00:20:19 +0100

    Only export certain names from the scrap module

--- scrap/__init__.py
+++ scrap/__init__.py
@@ -1,234 +1,234 @@
-from typing import Dict, Any, NamedTuple, Type, Optional, Callable, List
-from wrapper.renderable import Renderable, Void
-import pygame
-import inspect
+def _():
+    from typing import Dict, Any, NamedTuple, Type, Optional, Callable, List
+    from wrapper.renderable import Renderable, Void
+    import pygame
+    import inspect
 
+    class Scrap:
+        _DEFINITION: "Definition"
 
-class Scrap:
-    _DEFINITION: "Definition"
+        def __init__(self, *args, **kwargs):
+            self._latent = _resolve_arguments(args, kwargs, self._DEFINITION.latent)
+            self._derived: Dict[str, Any] = {}
 
-    def __init__(self, *args, **kwargs):
-        self._latent = _resolve_arguments(args, kwargs, self._DEFINITION.latent)
-        self._derived: Dict[str, Any] = {}
+        def __getattr__(self, attribute: str) -> Any:
+            # Assume attribute has at least one character
+            if attribute[0].islower():
+                if attribute in self._latent:
+                    return self._latent[attribute]
+                if attribute not in self._derived:
+                    self._derived[attribute] = self._DEFINITION.derived[attribute](self)
+                return self._derived[attribute]
 
-    def __getattr__(self, attribute: str) -> Any:
-        # Assume attribute has at least one character
-        if attribute[0].islower():
-            if attribute in self._latent:
-                return self._latent[attribute]
-            if attribute not in self._derived:
-                self._derived[attribute] = self._DEFINITION.derived[attribute](self)
-            return self._derived[attribute]
+            if attribute[0].isupper():
+                return lambda *args, **kwargs: self._handle(
+                    getattr(Registry, attribute)(*args, **kwargs)
+                )
 
-        if attribute[0].isupper():
-            return lambda *args, **kwargs: self._handle(
-                getattr(Registry, attribute)(*args, **kwargs)
+            raise AttributeError(attribute)
+
+        def _handle(self, event: "Scrap") -> "Scrap":
+            assert isinstance(event, Scrap)
+            if isinstance(event, Registry.get("Void")):
+                return self
+
+            preprocessed_event = _identity(self._DEFINITION.handlers.preprocessor)(
+                self, event
+            )
+            handling_function = self._DEFINITION.handlers.specific.get(
+                preprocessed_event._DEFINITION.name,
+                _identity(self._DEFINITION.handlers.fallback),
+            )
+            result = _identity(self._DEFINITION.handlers.postprocessor)(
+                self, handling_function(self, preprocessed_event),
+            )
+            assert isinstance(result, Scrap)
+            return result
+
+        def _cache(self) -> Renderable:
+            """Return a cached version of the rendered view of this scrap."""
+            return Void()
+
+        def _render(self, surface: pygame.Surface):
+            """Render the scrap to a pygame surface."""
+            self.cache().render(surface)
+
+        def __str__(self) -> str:
+            return (
+                self._DEFINITION.name
+                + "("
+                + ", ".join(
+                    [
+                        f"{name}={self._latent[name]}"
+                        for name, *_ in self._DEFINITION.latent
+                    ]
+                )
+                + ")"
             )
 
-        raise AttributeError(attribute)
+    class Builder(type):
+        def __getattr__(cls, attribute: str) -> Any:
+            constructor = cls._CONSTRUCTOR_LOOKUP[attribute]
+            return constructor
 
-    def _handle(self, event: "Scrap") -> "Scrap":
-        assert isinstance(event, Scrap)
-        if isinstance(event, Registry.get("Void")):
-            return self
+    class Registry(metaclass=Builder):
 
-        preprocessed_event = _identity(self._DEFINITION.handlers.preprocessor)(
-            self, event
-        )
-        handling_function = self._DEFINITION.handlers.specific.get(
-            preprocessed_event._DEFINITION.name,
-            _identity(self._DEFINITION.handlers.fallback),
-        )
-        result = _identity(self._DEFINITION.handlers.postprocessor)(
-            self, handling_function(self, preprocessed_event),
-        )
-        assert isinstance(result, Scrap)
-        return result
+        _CONSTRUCTOR_LOOKUP: Dict[str, Type[Scrap]] = {}
+        _ALLOW_DEFINITIONS: bool = False
 
-    def _cache(self) -> Renderable:
-        """Return a cached version of the rendered view of this scrap."""
-        return Void()
+        @classmethod
+        def get(cls, name: str) -> Type[Scrap]:
+            return cls._CONSTRUCTOR_LOOKUP[name]
 
-    def _render(self, surface: pygame.Surface):
-        """Render the scrap to a pygame surface."""
-        self.cache().render(surface)
+        @classmethod
+        def register(cls, definition: "Definition"):
+            constructor = constructor_from_definition(definition)
+            # TODO: assert that registrations are allowed
+            assert issubclass(constructor, Scrap)
+            assert definition.name != "Scrap"
+            assert definition.name not in cls._CONSTRUCTOR_LOOKUP
+            cls._CONSTRUCTOR_LOOKUP[definition.name] = constructor
 
-    def __str__(self) -> str:
-        return (
-            self._DEFINITION.name
-            + "("
-            + ", ".join(
-                [f"{name}={self._latent[name]}" for name, *_ in self._DEFINITION.latent]
+        @classmethod
+        def verify(cls):
+            def assert_valid_name(
+                candidate: str, *, lower: bool = False, upper: bool = False
+            ):
+                assert isinstance(candidate, str)
+                assert len(candidate) > 0
+                character = candidate[0]
+                assert character.isidentifier()
+                assert character != "_"
+                if lower:
+                    assert character.islower()
+                if upper:
+                    assert character.isupper()
+
+            for name, constructor in cls._CONSTRUCTOR_LOOKUP.items():
+                assert_valid_name(name, upper=True)
+                definition = constructor._DEFINITION
+                assert definition.name == name
+
+                requires_default = False
+                for latent in definition.latent:
+                    assert_valid_name(latent.name, lower=True)
+                    if requires_default:
+                        assert latent.optional
+                    if latent.optional:
+                        requires_default = True
+
+                for handler in definition.handlers.specific.keys():
+                    cls.get(handler)  # Throws error if the handler does not exist
+
+    Handler = Callable[[Scrap, Scrap], Scrap]
+    Derived = Callable[[Scrap], Scrap]
+
+    class Latent(NamedTuple):
+        name: str
+        kind: Type
+        optional: bool
+        default: Optional[Any]
+
+    class Handlers(NamedTuple):
+        preprocessor: Optional[Handler]
+        specific: Dict[str, Handler]
+        fallback: Optional[Handler]
+        postprocessor: Optional[Handler]
+
+    class Definition(NamedTuple):
+        name: str
+        latent: List[Latent]
+        derived: Dict[str, Derived]
+        handlers: Handlers
+        cache: Optional[Callable[[], Renderable]]
+        render: Optional[Callable[[pygame.Surface], None]]
+
+    def definition_from_constructor(constructor: Type) -> Definition:
+        attributes = constructor.__dict__
+
+        derived = {}
+        specific = {}
+
+        latent = [
+            Latent(
+                name,
+                annotation,
+                name in constructor.__dict__,
+                constructor.__dict__.get(name),
             )
-            + ")"
+            for name, annotation in attributes.get("__annotations__", {}).items()
+        ]
+        skip = {name for name, *_ in latent}
+
+        for key, value in attributes.items():
+            if key.startswith("__") or key in skip:
+                continue
+
+            if key[0].islower():
+                assert (
+                    type(value).__name__ == "function"
+                ), f"Missing type annotation for {key}"
+                derived[key] = value
+
+            if key[0].isupper():
+                assert type(value).__name__ == "function"
+                specific[key] = value
+
+        return Definition(
+            constructor.__name__,
+            latent,
+            derived,
+            Handlers(
+                attributes.get("_preprocessor", None),
+                specific,
+                attributes.get("_fallback", None),
+                attributes.get("_postprocessor", None),
+            ),
+            attributes.get("_cache", None),
+            attributes.get("_render", None),
         )
 
+    def constructor_from_definition(definition: Definition) -> Type:
+        attributes = {"_DEFINITION": definition}
 
-class Builder(type):
-    def __getattr__(cls, attribute: str) -> Any:
-        constructor = cls._CONSTRUCTOR_LOOKUP[attribute]
-        return constructor
+        if definition.cache is not None:
+            attributes["_cache"] = definition.cache
 
+        if definition.render is not None:
+            attributes["_render"] = definition.cache
 
-class Registry(metaclass=Builder):
+        return type(definition.name, (Scrap,), attributes)
 
-    _CONSTRUCTOR_LOOKUP: Dict[str, Type[Scrap]] = {}
-    _ALLOW_DEFINITIONS: bool = False
+    def scrap(constructor: Type) -> Type[Scrap]:
+        definition = definition_from_constructor(constructor)
+        Registry.register(definition)
+        return Registry.get(definition.name)
 
-    @classmethod
-    def get(cls, name: str) -> Type[Scrap]:
-        return cls._CONSTRUCTOR_LOOKUP[name]
+    def _resolve_arguments(
+        args: tuple, kwargs: dict, latent: List[Latent]
+    ) -> Dict[str, Any]:
+        arguments = {}
 
-    @classmethod
-    def register(cls, definition: "Definition"):
-        constructor = constructor_from_definition(definition)
-        # TODO: assert that registrations are allowed
-        assert issubclass(constructor, Scrap)
-        assert definition.name not in cls._CONSTRUCTOR_LOOKUP
-        cls._CONSTRUCTOR_LOOKUP[definition.name] = constructor
+        for i, (name, _, optional, default) in enumerate(latent):
+            if i < len(args):
+                arguments[name] = args[i]
+                assert name not in kwargs
+            else:
+                assert optional
+                arguments[name] = kwargs.get(name, default)
 
-    @classmethod
-    def verify(cls):
-        def assert_valid_name(
-            candidate: str, *, lower: bool = False, upper: bool = False
-        ):
-            assert isinstance(candidate, str)
-            assert len(candidate) > 0
-            character = candidate[0]
-            assert character.isidentifier()
-            assert character != "_"
-            if lower:
-                assert character.islower()
-            if upper:
-                assert character.isupper()
+        return arguments
 
-        for name, constructor in cls._CONSTRUCTOR_LOOKUP.items():
-            assert_valid_name(name, upper=True)
-            definition = constructor._DEFINITION
-            assert definition.name == name
+    def _identity(
+        function: Optional[Callable[[Any, Any], Any]]
+    ) -> Callable[[Any, Any], Any]:
+        return function if function is not None else (lambda _, other: other)
 
-            requires_default = False
-            for latent in definition.latent:
-                assert_valid_name(latent.name, lower=True)
-                if requires_default:
-                    assert latent.optional
-                if latent.optional:
-                    requires_default = True
-
-            for handler in definition.handlers.specific.keys():
-                cls.get(handler)  # Throws error if the handler does not exist
+    return {"scrap": scrap, "Scrap": scrap, **Registry._CONSTRUCTOR_LOOKUP}
 
 
-Handler = Callable[[Scrap, Scrap], Scrap]
-Derived = Callable[[Scrap], Scrap]
-
-
-class Latent(NamedTuple):
-    name: str
-    kind: Type
-    optional: bool
-    default: Optional[Any]
-
-
-class Handlers(NamedTuple):
-    preprocessor: Optional[Handler]
-    specific: Dict[str, Handler]
-    fallback: Optional[Handler]
-    postprocessor: Optional[Handler]
-
-
-class Definition(NamedTuple):
-    name: str
-    latent: List[Latent]
-    derived: Dict[str, Derived]
-    handlers: Handlers
-    cache: Optional[Callable[[], Renderable]]
-    render: Optional[Callable[[pygame.Surface], None]]
-
-
-def definition_from_constructor(constructor: Type) -> Definition:
-    attributes = constructor.__dict__
-
-    derived = {}
-    specific = {}
-
-    latent = [
-        Latent(
-            name,
-            annotation,
-            name in constructor.__dict__,
-            constructor.__dict__.get(name),
-        )
-        for name, annotation in attributes.get("__annotations__", {}).items()
-    ]
-    skip = {name for name, *_ in latent}
-
-    for key, value in attributes.items():
-        if key.startswith("__") or key in skip:
-            continue
-
-        if key[0].islower():
-            assert (
-                type(value).__name__ == "function"
-            ), f"Missing type annotation for {key}"
-            derived[key] = value
-
-        if key[0].isupper():
-            assert type(value).__name__ == "function"
-            specific[key] = value
-
-    return Definition(
-        constructor.__name__,
-        latent,
-        derived,
-        Handlers(
-            attributes.get("_preprocessor", None),
-            specific,
-            attributes.get("_fallback", None),
-            attributes.get("_postprocessor", None),
-        ),
-        attributes.get("_cache", None),
-        attributes.get("_render", None),
-    )
-
-
-def constructor_from_definition(definition: Definition) -> Type:
-    attributes = {"_DEFINITION": definition}
-
-    if definition.cache is not None:
-        attributes["_cache"] = definition.cache
-
-    if definition.render is not None:
-        attributes["_render"] = definition.cache
-
-    return type(definition.name, (Scrap,), attributes)
-
-
-def scrap(constructor: Type) -> Type[Scrap]:
-    definition = definition_from_constructor(constructor)
-    Registry.register(definition)
-
-
-def _resolve_arguments(
-    args: tuple, kwargs: dict, latent: List[Latent]
-) -> Dict[str, Any]:
-    arguments = {}
-
-    for i, (name, _, optional, default) in enumerate(latent):
-        if i < len(args):
-            arguments[name] = args[i]
-            assert name not in kwargs
-        else:
-            assert optional
-            arguments[name] = kwargs.get(name, default)
-
-    return arguments
-
-
-def _identity(
-    function: Optional[Callable[[Any, Any], Any]]
-) -> Callable[[Any, Any], Any]:
-    return function if function is not None else (lambda _, other: other)
+for _key, _value in _().items():
+    globals()[_key] = _value
 
 
 # from utils.serialisation import deserialise_path as _deserialise
