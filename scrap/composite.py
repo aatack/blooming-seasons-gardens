da8dc85f30e1bb1a9b10df5302f890de11c345fe
aatack <alexanderjtatack@gmail.com>
2021-08-14 18:57:45 +0100

    Implement a click catcher

--- scrap/composite.py
+++ scrap/composite.py
@@ -1,7 +1,10 @@
 from scrap.base import Scrap
 from scrap.data import Message, Literal
 from scrap.queries import Contains
+from scrap.impure import Timer
+from scrap.event import Button, Click
 import wrapper.renderable as renderable
+import pygame
 from typing import List, Optional
 
 
@@ -58,3 +61,89 @@ class Group(Scrap):
 
     def __str__(self) -> str:
         return f"[{', '.join(str(child) for child in self.children)}]"
+
+
+class CatchClicks(Scrap):
+    def __init__(
+        self,
+        scrap: Scrap,
+        minimum_time: Optional[int] = None,
+        button: int = 1,
+        timer: Optional[Timer] = None,
+    ):
+        self.scrap = scrap
+        self.minimum_time = minimum_time
+        self.button = button
+        self.timer = timer
+
+    def cache(self) -> renderable.Renderable:
+        """Return a cached version of the rendered view of this scrap."""
+        return self.scrap.cache()
+
+    def render(self, surface: pygame.Surface):
+        """Render the scrap to a pygame surface."""
+        self.scrap.render(surface)
+
+    def handle(self, event: Scrap) -> Scrap:
+        timer = self.timer
+
+        if isinstance(event, Button) and event.button == self.button:
+            contains = self.scrap.handle(Contains(event.location))
+            assert isinstance(contains, Message)
+            hit = (
+                True
+                if (
+                    isinstance(contains.message, Literal)
+                    and (contains.message.value is True)
+                )
+                else False
+            )
+
+            if event.down and hit:
+                if self.minimum_time is None:
+                    scrap = self.scrap.handle(Click(self.button, event.location))
+                else:
+                    return CatchClicks(
+                        self.scrap,
+                        minimum_time=self.minimum_time,
+                        button=self.button,
+                        timer=Timer.new(),
+                    )
+            elif event.up and hit:
+                if self.timer is None or self.timer.time < self.minimum_time:
+                    return self
+                else:
+                    scrap = self.scrap.handle(Click(self.button, event.location))
+            else:
+                return CatchClicks(
+                    self.scrap, minimum_time=self.minimum_time, button=self.button,
+                )
+        else:
+            scrap = self.scrap.handle(event)
+
+        if scrap is self.scrap:
+            return self
+
+        if isinstance(scrap, Message):
+            return Message(
+                CatchClicks(
+                    scrap.scrap,
+                    minimum_time=self.minimum_time,
+                    button=self.button,
+                    timer=self.timer,
+                )
+                if scrap.scrap is not self.scrap
+                else self,
+                scrap.message,
+            )
+        else:
+            return (
+                CatchClicks(
+                    scrap,
+                    minimum_time=self.minimum_time,
+                    button=self.button,
+                    timer=timer,
+                )
+                if scrap is not self.scrap
+                else self
+            )
