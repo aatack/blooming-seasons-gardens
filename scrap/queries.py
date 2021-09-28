ad27206dee6732e122794a08ddc171d9bd3391a0
aatack <alexanderjtatack@gmail.com>
2021-09-28 23:27:43 +0100

    Define scraps for caching and rendering

--- scrap/queries.py
+++ scrap/queries.py
@@ -2,6 +2,19 @@ from scrap.base import defscrap, Scrap
 from scrap.data import Point, Literal
 from scrap.composite import Group
 from typing import List
+import pygame
+
+
+@defscrap
+class Cache:
+    """Used to request a cached version of the scrap for rendering efficiently."""
+
+
+@defscrap
+class Render:
+    """Used to request that the scrap be rendered to the screen."""
+
+    surface: pygame.Surface
 
 
 @defscrap
