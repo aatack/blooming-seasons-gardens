94ebde310fe75b24330a586205df0e32c2f25083
aatack <alexanderjtatack@gmail.com>
2021-11-09 22:52:09 +0000

    Define view simplification

--- view.py
+++ view.py
@@ -1,41 +1,24 @@
-import abc
 import pygame
-from typing import Tuple
+from typing import Tuple, Union, List
 
 
-class View(abc.ABC):
-    """
-    Abstract representation of a view of a 2D user interface.
-    
-    Used to simplify nested representations, preventing translations with negative
-    offsets from cutting off some of the view during their resolution.
-    """
-
-    @abc.abstractmethod
-    def resolve(self) -> pygame.Surface:
-        """Resolve the view into a pygame surface."""
-
-    @abc.abstractmethod
-    def size(self) -> Tuple[int, int]:
-        """Return the size of the view in pixels."""
+View = Union[pygame.Surface, Tuple[float, float, "View"], List["View"]]
 
 
-class Literal(View):
-    def __init__(self, surface: pygame.Surface):
-        self.surface = surface
-
-    def resolve(self) -> pygame.Surface:
-        return self.surface
-
-    def size(self) -> Tuple[int, int]:
-        return self.surface.get_size()
-
-
-class Translate(View):
-    def __init__(self, view: View, x: float, y: float):
-        self.view = view
-        self.x = x
-        self.y = y
-
-    def resolve(self) -> pygame.Surface:
-        raise NotImplementedError()
+def simplify(view: View) -> List[Tuple[float, float, View]]:
+    """Simplify any nested hierarchy of views into a list of translated literals."""
+    if isinstance(view, pygame.Surface):
+        return [(0.0, 0.0, view)]
+    elif isinstance(view, tuple):
+        outer_x, outer_y, inner_view = view
+        return [
+            (outer_x + inner_x, outer_y + inner_y, surface)
+            for inner_x, inner_y, surface in simplify(inner_view)
+        ]
+    elif isinstance(view, list):
+        result = []
+        for inner_view in view:
+            result.extend(simplify(inner_view))
+        return result
+    else:
+        raise ValueError(f"Unexpected view: {view} of type {type(view)}")
