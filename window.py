b11807f10c5af143ec1749d694bdbbc0da05a75c
aatack <alexanderjtatack@gmail.com>
2021-11-14 01:43:21 +0000

    Update the way the window runs

--- window.py
+++ window.py
@@ -11,35 +11,31 @@ pygame.init()
 class Window:
     def __init__(
         self,
-        state: Optional[State] = None,
-        width: int = 500,
-        height: int = 500,
-        background_colour: Colour = Colour(red=1.0, green=1.0, blue=1.0),
         title: Optional[str] = None,
+        background_colour: Colour = Colour(red=1.0, green=1.0, blue=1.0),
     ):
         pygame.init()
 
-        self.state = state
-        self.view = Derived(simplify, self.state.view())
         self.background_colour = background_colour
 
-        self.width = Variable(width)
-        self.height = Variable(height)
+        self.title = title
+        if self.title is not None:
+            pygame.display.set_caption(self.title)
+
+        self.width = Variable(500)
+        self.height = Variable(500)
         self.mouse = Point(0, 0)
 
         self.surface = pygame.display.set_mode(
             (self.width.value(), self.height.value()), pygame.RESIZABLE
         )
 
-        self.title = title
-        if self.title is not None:
-            pygame.display.set_caption(self.title)
-
-    def run(self):
-        while self.loop():
+    def run(self, state: State):
+        view = Derived(simplify, state.view())
+        while self.loop(state, view):
             pass
 
-    def loop(self) -> bool:
+    def loop(self, state: State, view: State) -> bool:
         for event in pygame.event.get():
             if event.type == pygame.VIDEORESIZE:
                 self.surface = pygame.display.set_mode(
@@ -52,24 +48,24 @@ class Window:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     return False
-                self.state.key(event.key, True)
+                state.key(event.key, True)
             if event.type == pygame.KEYUP:
-                self.state.key(event.key, False)
+                state.key(event.key, False)
             if event.type == pygame.MOUSEBUTTONDOWN:
                 # TODO: work out why the position in the event does not match the
                 #       position in the stored state if the mouse is clicked while it is
                 #       moving
-                self.state.mouse(event.button, event.pos, True)
+                state.mouse(event.button, event.pos, True)
             if event.type == pygame.MOUSEBUTTONUP:
-                self.state.mouse(event.button, event.pos, False)
+                state.mouse(event.button, event.pos, False)
             if event.type == pygame.MOUSEMOTION:
                 self.mouse.x = event.pos[0]
                 self.mouse.y = event.pos[1]
 
         self.surface.fill(self.background_colour.colour_cache)
-        view = self.view.value()
-        if view is not None:
-            render(view, self.surface, simplified=True)
+        view_value = view.value()
+        if view_value is not None:
+            render(view_value, self.surface, simplified=True)
 
         pygame.display.flip()
         return True
