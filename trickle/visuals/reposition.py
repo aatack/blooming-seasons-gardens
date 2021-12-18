32d3d9f787fc32c4fe64589c0a2d8f25528bfa4f
aatack <alexanderjtatack@gmail.com>
2021-12-18 16:40:34 +0000

    Reparameterise repositioned visuals

--- trickle/visuals/reposition.py
+++ trickle/visuals/reposition.py
@@ -9,18 +9,38 @@ class Reposition(Visual):
     def __init__(
         self,
         visual: Visual,
+        *,
+        crop_bottom: Optional[float] = None,
+        crop_right: Optional[float] = None,
+        crop_top: float = 0.0,
+        crop_left: float = 0.0,
         horizontal_offset: float = 0.0,
         vertical_offset: float = 0.0,
-        horizontal_crop: Optional[float] = None,
-        vertical_crop: Optional[float] = None,
     ):
+        """
+        Represents a repositioned visual.
+
+        The child visual is first rendered, then cropped.  After cropping the remaining
+        visual is moved according to the specified offsets.
+        """
         self.visual = visual
+
+        self.crop_bottom = crop_bottom
+        self.crop_right = crop_right
+        self.crop_top = crop_top
+        self.crop_left = crop_left
 
         self.horizontal_offset = horizontal_offset
         self.vertical_offset = vertical_offset
 
-        self.horizontal_crop = horizontal_crop
-        self.vertical_crop = vertical_crop
+        assert self.crop_top >= 0.0
+        assert self.crop_left >= 0.0
+
+        # Under certain circumstances the crops will overlap, which will result in no
+        # visual being rendered at all
+        self.empty = (
+            (self.crop_bottom is not None) and self.crop_bottom <= self.crop_top
+        ) or ((self.crop_right is not None) and self.crop_right <= self.crop_left)
 
     def simplify(self) -> "Visual":
         simplified_visual = self.visual.simplify()
@@ -29,15 +49,8 @@ class Reposition(Visual):
         visuals = []
         for visual in simplified_visual.visuals:
             assert isinstance(visual, Reposition)
-            visuals.append(
-                Reposition(
-                    visual.visual,
-                    self.horizontal_offset + visual.horizontal_offset,
-                    self.vertical_offset + visual.vertical_offset,
-                    _compound_crops(self.horizontal_crop, visual.horizontal_crop),
-                    _compound_crops(self.vertical_crop, visual.vertical_crop),
-                )
-            )
+
+            raise NotImplementedError()
 
         return Overlay(*visuals)
 
@@ -50,30 +63,39 @@ class Reposition(Visual):
 
         base_surface = Surface.empty(
             self.visual.horizontal_extent()
-            if self.horizontal_crop is None
-            else self.horizontal_crop,
+            if self.crop_right is None
+            else self.crop_right,
             self.visual.vertical_extent()
-            if self.vertical_crop is None
-            else self.vertical_crop,
+            if self.crop_bottom is None
+            else self.crop_bottom,
             transparent=True,
         )
         self.visual.render(base_surface)
 
-        surface.blit(base_surface, (self.horizontal_offset, self.vertical_offset))
+        crop_position = (int(self.crop_left), int(self.crop_top))
+        crop_size = tuple(
+            base - crop for base, crop in zip(base_surface.get_size(), crop_position)
+        )
+
+        surface.blit(
+            base_surface,
+            (int(self.horizontal_offset), int(self.vertical_offset)),
+            crop_position + crop_size,
+        )
 
     def horizontal_extent(self) -> float:
-        return self.horizontal_offset + (
-            self.horizontal_crop
-            if self.horizontal_crop is not None
-            else self.visual.horizontal_extent()
-        )
+        extent = self.visual.horizontal_extent()
+        if self.crop_right is not None:
+            extent = min(extent, self.crop_right)
+        extent -= self.crop_left
+        return extent + self.horizontal_offset
 
     def vertical_extent(self) -> float:
-        return self.vertical_offset + (
-            self.vertical_crop
-            if self.vertical_crop is not None
-            else self.visual.vertical_extent()
-        )
+        extent = self.visual.vertical_extent()
+        if self.crop_bottom is not None:
+            extent = min(extent, self.crop_bottom)
+        extent -= self.crop_top
+        return extent + self.vertical_offset
 
 
 def _compound_crops(left: Optional[float], right: Optional[float]) -> Optional[float]:
