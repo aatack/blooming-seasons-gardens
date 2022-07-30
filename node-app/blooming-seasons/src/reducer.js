8c26ac3689e6b187d3ead9050a4c611b471bc82f
aatack <alexanderjtatack@gmail.com>
2022-07-30 22:12:11 +0100

    Get rid of the old initial state

--- node-app/blooming-seasons/src/reducer.js
+++ node-app/blooming-seasons/src/reducer.js
@@ -2,18 +2,6 @@ import { combineReducers } from "redux"
 import reduceTodos from './features/todos'
 import reduceFilters from './features/filters'
 
-const initialState = {
-    todos: [
-        { id: 0, text: 'Learn React', completed: true },
-        { id: 1, text: 'Learn Redux', completed: false, color: 'purple' },
-        { id: 2, text: 'Build something fun!', completed: false, color: 'blue' }
-    ],
-    filters: {
-        status: 'All',
-        colors: []
-    }
-}
-
 const rootReducer = combineReducers({
     todos: reduceTodos, filtrers: reduceFilters
 })
