d3e59da48cdcd7e799095a7e2f519b9c6814e6a9
aatack <alexanderjtatack@gmail.com>
2022-07-30 14:30:22 +0100

    Work through the form tutorial

--- node-app/blooming-seasons/src/index.js
+++ node-app/blooming-seasons/src/index.js
@@ -1,9 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-function App() {
-    return <p>Hello World</p>
+class NameForm extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { value: ["Initial text"] };
+    }
+
+    handleChange = (event) => {
+        this.setState({ value: event.target.value });
+    }
+
+    handleSubmit = (event) => {
+        alert("A name was submitted: " + this.state.value);
+        event.preventDefault();
+    }
+
+    report = () => {
+        alert("CLICKED")
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Name:
+                    <textarea value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <select multiple={true} value={this.state.value} onChange={this.handleChange}>
+                    <option value="red">Red</option>
+                    <option value="blue">Blue</option>
+                    <option value="yellow">Yellow</option>
+                    <option value="green">Green</option>
+                </select>
+                <p onClick={this.report} hover="true">{this.state.value}</p>
+                <input type="submit" />
+                <input type="file" />
+            </form>
+        );
+    }
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />)
+root.render(<NameForm />)
