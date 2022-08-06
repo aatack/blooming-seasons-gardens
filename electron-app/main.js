bd7e0f8c378364c0c22236ffd97970d80754b023
aatack <alexanderjtatack@gmail.com>
2022-08-06 01:13:18 +0100

    Add the platform-specific boilerplate

--- electron-app/main.js
+++ electron-app/main.js
@@ -1,13 +1,29 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
 const createWindow = () => {
-    const window = new BrowserWindow({
-        width: 800, height: 600
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
     })
 
-    window.loadFile("index.html")
+    mainWindow.loadFile('index.html')
+
+    // Open developer tools
+    mainWindow.webContents.openDevTools()
 }
 
 app.whenReady().then(() => {
-    createWindow();
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
 })
