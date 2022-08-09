a8c564de74ea71b62a121cdd5a46efedfa7f5b31
aatack <alexanderjtatack@gmail.com>
2022-08-10 00:37:43 +0100

    Use immer to simplify state reduction

--- electron-app/src/model/store.js
+++ electron-app/src/model/store.js
@@ -1,22 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
+import produce from "immer";
 
 export const store = configureStore({
   reducer: (state = { identifier: 0, garden: [], nursery: [] }, action) => {
     switch (action.type) {
       case "garden/bed/added":
-        return {
-          ...state,
-          identifier: state.identifier + 1,
-          garden: [
-            ...state.garden,
-            {
-              identifier: state.identifier,
-              name: action.payload
-                ? action.payload
-                : "Bed " + state.identifier.toString(),
-            },
-          ],
-        };
+        return produce(state, (draft) => {
+          draft.identifier += 1;
+          draft.garden.push({
+            identifier: state.identifier,
+            name: action.payload
+              ? action.payload
+              : "Bed " + state.identifier.toString(),
+          });
+        });
       default:
         return state;
     }
