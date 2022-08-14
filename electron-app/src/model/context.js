11c0304b34cf314324af2cf9ad8af5ac8cff5f54
aatack <alexanderjtatack@gmail.com>
2022-08-14 22:00:47 +0100

    Use local scope when setting up the modal

--- electron-app/src/model/context.js
+++ electron-app/src/model/context.js
@@ -1,28 +1,31 @@
 import React, { useState } from "react";
 
-export const Modal = React.createContext(undefined);
+export const Modal = (() => {
+  const Modal = React.createContext(undefined);
 
-const CachedModalProvider = Modal.Provider;
+  const CachedModalProvider = Modal.Provider;
 
-const useModal = () => {
-  const [modal, privateSetModal] = useState(null);
+  const useModal = () => {
+    const [modal, privateSetModal] = useState(null);
 
-  const publicSetModal = (newModal) => {
-    if (modal && modal.onClose) {
-      modal.onClose();
-    }
-    privateSetModal(newModal);
+    const publicSetModal = (newModal) => {
+      if (modal && modal.onClose) {
+        modal.onClose();
+      }
+      privateSetModal(newModal);
+    };
+
+    return [modal, publicSetModal];
   };
 
-  return [modal, publicSetModal];
-};
+  const WrappedModalProvider = (props) => {
+    return (
+      <CachedModalProvider value={useModal()}>
+        {props.children}
+      </CachedModalProvider>
+    );
+  };
 
-const WrappedModalProvider = (props) => {
-  return (
-    <CachedModalProvider value={useModal()}>
-      {props.children}
-    </CachedModalProvider>
-  );
-};
-
-Modal.Provider = WrappedModalProvider;
+  Modal.Provider = WrappedModalProvider;
+  return Modal;
+})();
