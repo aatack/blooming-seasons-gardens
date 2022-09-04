dac83e935c0b1acfd964f22854785be15f494b58
aatack <alexanderjtatack@gmail.com>
2022-09-04 03:22:07 +0100

    Tidy up the code a little

--- electron-app/src/view/common/rendering.js
+++ electron-app/src/view/common/rendering.js
@@ -14,19 +14,16 @@ export const SVGViewer = ({
   const [y, setY] = useState(initialY || 0);
   const [scale, setScale] = useState(initialScale || 1);
 
-  const [initialPosition, setInitialPosition] = useState(null);
-
   const [dragX, setDragX] = useState(0);
   const [dragY, setDragY] = useState(0);
-  const [dragScale, setDragScale] = useState(false);
   const [dragging, setDragging] = useState(false);
 
-  const doMove = (dx, dy) => {
+  const translate = (dx, dy) => {
     setX(x + dx);
     setY(y + dy);
   };
 
-  const doScale = (fixedX, fixedY, zoom) => {
+  const zoom = (fixedX, fixedY, zoom) => {
     const newScale = clamp(scale * zoom, 1 / 100, 100);
     const actualZoom = newScale / scale;
 
@@ -36,55 +33,9 @@ export const SVGViewer = ({
     setScale(newScale);
   };
 
-  const handleDragOver = (e) => {
-    e.preventDefault();
-    e.dataTransfer.dropEffect = "move";
-  };
-
-  const handleDragStart = (e) => {
-    console.log("Dragging start");
-    // Use an empty image as the ghost image that gets displayed during dragging
-    // e.dataTransfer.setDragImage(
-    //   document.createElement("img"),
-    //   window.outerWidth,
-    //   window.outerHeight
-    // );
-
-    setDragX(e.clientX);
-    setDragY(e.clientY);
-
-    setDragScale(e.ctrlKey);
-
-    const offset = e.target.getBoundingClientRect();
-    if (initialPosition === null) {
-      setInitialPosition({
-        x: e.clientX - offset.x,
-        y: e.clientY - offset.y,
-      });
-    }
-  };
-
   const handleClick = () => {
-    console.log("On click");
     if (onClick) {
       onClick();
-    }
-  };
-
-  const handleDrag = (e) => {
-    if (e.clientX !== 0 && e.clientY !== 0) {
-      if (dragScale) {
-        doScale(
-          initialPosition.x / scale - x,
-          initialPosition.y / scale - y,
-          1 + -(e.clientY - dragY) * 0.005
-        );
-      } else {
-        doMove((e.clientX - dragX) / scale, (e.clientY - dragY) / scale);
-      }
-
-      setDragX(e.clientX);
-      setDragY(e.clientY);
     }
   };
 
@@ -92,22 +43,26 @@ export const SVGViewer = ({
     if (e.buttons == 1) {
       if (!dragging) {
         setDragging(true);
-        handleDragStart(e);
+        setDragX(e.clientX);
+        setDragY(e.clientY);
       }
     } else if (dragging) {
       setDragging(false);
-      setInitialPosition(null);
     }
 
     if (dragging) {
-      handleDrag(e);
+      if (e.clientX !== 0 && e.clientY !== 0) {
+        translate((e.clientX - dragX) / scale, (e.clientY - dragY) / scale);
+
+        setDragX(e.clientX);
+        setDragY(e.clientY);
+      }
     }
   };
 
   const handleWheel = (e) => {
     const offset = svgElement.current.getBoundingClientRect();
-    console.log(e.deltaY);
-    doScale(
+    zoom(
       (e.clientX - offset.x) / scale - x,
       (e.clientY - offset.y) / scale - y,
       1 - e.deltaY * 0.002
@@ -116,9 +71,9 @@ export const SVGViewer = ({
 
   return (
     <div
-      onWheel={handleWheel}
-      onMouseMove={handleMouseMove}
       onClick={handleClick}
+      onMouseMove={handleMouseMove}
+      onWheel={handleWheel}
       style={{ width: "100%", height: "100%" }}
     >
       <svg ref={svgElement} style={{ width: "100%", height: "100%" }}>
