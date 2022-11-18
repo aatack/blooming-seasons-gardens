9751ff022580480ff9d4aebb41a382db6bb905ea
aatack <alexanderjtatack@gmail.com>
2022-11-18 00:30:50 +0000

    Test a simple request

--- electron-app/server/index.js
+++ electron-app/server/index.js
@@ -4,6 +4,10 @@ const PORT = 3001;
 
 const app = express();
 
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello world" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
