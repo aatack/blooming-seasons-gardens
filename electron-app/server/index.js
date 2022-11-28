6fbb24207366582901ed0dcd182c01972e2c7ec6
aatack <alexanderjtatack@gmail.com>
2022-11-28 03:46:37 +0000

    Send a blank state if nothing exists yet

--- electron-app/server/index.js
+++ electron-app/server/index.js
@@ -3,14 +3,30 @@ const fs = require("fs");
 
 const PORT = 3001;
 
+const defaultState = {
+  garden: null,
+  history: {
+    index: 0,
+    items: [],
+  },
+  workspace: {
+    identifier: 1,
+    gardens: [],
+  },
+};
+
 const app = express();
 
 app.use(express.json({ limit: "1gb" }));
 
 app.get("/load", (request, response) => {
-  response.json({
-    message: JSON.parse(fs.readFileSync("data/store.json").toString()),
-  });
+  var message;
+  if (fs.existsSync("data/store.json")) {
+    message = JSON.parse(fs.readFileSync("data/store.json").toString());
+  } else {
+    message = defaultState;
+  }
+  response.json({ message: message });
 });
 
 app.post("/save", (request, response) => {
