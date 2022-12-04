6ac78c5cc968ddde5bc953b35752824097168466
aatack <alexanderjtatack@gmail.com>
2022-12-04 06:03:44 +0000

    Ensure the custom font can be used

--- electron-app/src/view/plan/label.js
+++ electron-app/src/view/plan/label.js
@@ -32,7 +32,7 @@ const Label = ({ label }) => {
         fill={
           isHovered ? HOVERED_COLOUR : isSelected ? SELECTED_COLOUR : "black"
         }
-        style={{ userSelect: "none", fontFamily: "arial" }}
+        style={{ userSelect: "none", fontFamily: "Spectral" }}
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
       >
