f0e949d38ba642c5e3788d5a3214b6b8ea6525f3
aatack <alexanderjtatack@gmail.com>
2023-04-08 15:45:40 +0100

    Set up a flutter app

---
+++ flutter-app/blooming_seasons_design_studio/README.md
@@ -0,0 +1,3 @@
+# blooming_seasons_design_studio
+
+A new Flutter project.
---
+++ flutter-app/blooming_seasons_design_studio/android/app/src/main/kotlin/com/example/blooming_seasons_design_studio/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.blooming_seasons_design_studio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
