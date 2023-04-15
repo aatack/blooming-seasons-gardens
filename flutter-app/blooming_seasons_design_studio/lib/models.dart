78a9a928dacd9a1079d830ceb0bb4c69d04f68da
aatack <alexanderjtatack@gmail.com>
2023-04-15 16:29:39 +0100

    Specify the different types of plant

--- flutter-app/blooming_seasons_design_studio/lib/models.dart
+++ flutter-app/blooming_seasons_design_studio/lib/models.dart
@@ -1,18 +1,21 @@
 import 'dart:collection';
 import 'dart:ffi';
+import 'package:image/image.dart' as img;
+
+import 'package:flutter/material.dart';
 
 class Garden {
   const Garden(this._beds, this._nursery);
 
-  final List<Bed> _beds;
-  UnmodifiableListView<Bed> get beds => UnmodifiableListView(_beds);
+  final List<Positioned<Bed>> _beds;
+  UnmodifiableListView<Positioned<Bed>> get beds => UnmodifiableListView(_beds);
 
   final List<Template> _nursery;
   UnmodifiableListView<Template> get nursery => UnmodifiableListView(_nursery);
 }
 
-class Element<T> {
-  const Element(this.id, this.x, this.y, this.element);
+class Positioned<T> {
+  const Positioned(this.id, this.x, this.y, this.element);
 
   final Int id;
   final Float x;
@@ -30,20 +33,46 @@ class Template {
 class Bed {
   const Bed(this._elements);
 
-  final List<Element> _elements;
-  UnmodifiableListView<Element> get elements => UnmodifiableListView(_elements);
+  final List<Positioned> _elements;
+  UnmodifiableListView<Positioned> get elements =>
+      UnmodifiableListView(_elements);
 }
 
 class Plant {
-  const Plant(this.name);
+  const Plant(this.name, this.size, this.plantType, this.border, this.image);
 
   final String name;
+
+  final Float size;
+
+  final PlantType plantType;
+  final PlantBorder? border;
+  final PlantImage? image;
+}
+
+enum PlantType { border, image }
+
+class PlantBorder {
+  const PlantBorder(this.thickness, this.colour);
+
+  final Float thickness;
+  final Color colour;
+}
+
+class PlantImage {
+  const PlantImage(this.image, this.x, this.y, this.scale);
+
+  final img.Image image;
+  final Float x;
+  final Float y;
+  final Float scale;
 }
 
 class Label {
-  const Label(this.text);
+  const Label(this.text, this.size);
 
   final String text;
+  final Float size;
 }
 
 class Arrow {
