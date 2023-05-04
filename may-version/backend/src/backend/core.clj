b3083c0fab4cf0a5e2154f9acf76b3428f815e92
aatack <alexanderjtatack@gmail.com>
2023-05-04 22:39:42 +0100

    Set up the basic endpoints

--- may-version/backend/src/backend/core.clj
+++ may-version/backend/src/backend/core.clj
@@ -2,15 +2,44 @@
   (:require [clojure.string :refer [split]]
             [ring.adapter.jetty :refer [run-jetty]]
             [ring.middleware.reload :refer [wrap-reload]]
-            [ring.util.response :refer [response]]))
+            [ring.util.response :refer [status content-type bad-request]]))
+
+(defn list-gardens []
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body "list-gardens"})
+
+(defn get-garden [name]
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body (str ["get-garden" name])})
+
+(defn put-garden [name content]
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body (str ["put-garden" name content])})
+
+(defn delete-garden [name]
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body (str ["delete-garden" name])})
+
+(defn rename-garden [old-name new-name]
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body (str ["rename-garden" old-name new-name])})
 
 (defn handler [request]
   (let [segments (filter not-empty (split (:uri request) #"/"))]
-    (response
-     (cond
-       (= segments ["inspect"]) (str request)
-       (= segments ["gardens"]) "gardens"
-       :else (str "not known" segments request)))))
+    (cond
+      (= segments ["inspect"]) (bad-request (str request))
+      (and (= segments ["garden"])
+           (= (:request-method request) :get)
+           (= (count segments) 1)) (list-gardens)
+      (and (= (first segments) "garden")
+           (= (:request-method request) :get)
+           (= (count segments) 2)) (get-garden (second segments))
+      :else (bad-request (apply str "not-found: " segments)))))
 
 (def app
   (wrap-reload #'handler))
