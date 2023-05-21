52446f191924afe5bc932c9996f0ada1a059aff3
aatack <alexanderjtatack@gmail.com>
2023-05-21 23:27:19 +0100

    Clear modals when the surrounding blank space is clicked

--- flutter-app/blooming_seasons_design_studio/lib/widgets/providers/modals.dart
+++ flutter-app/blooming_seasons_design_studio/lib/widgets/providers/modals.dart
@@ -10,6 +10,9 @@ class ModalsProvider extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // NOTE: for whatever reason, this implementation creates an annoying
+    //       flickering whenever the presence of some modal changes
+
     return BlocProvider<ModalsState>(
       create: (_) => ModalsState(),
       child: BlocBuilder<ModalsState, List<Widget>>(
@@ -20,12 +23,21 @@ class ModalsProvider extends StatelessWidget {
             return Stack(
               children: [
                 child,
-                Scaffold(
-                  backgroundColor: Colors.grey[700]!.withOpacity(0.5),
-                  body: Center(
-                    child: Container(
-                      color: Colors.white,
-                      child: state.last,
+                GestureDetector(
+                  onTap: () {
+                    context.read<ModalsState>().clear();
+                  },
+                  child: Scaffold(
+                    backgroundColor: Colors.grey[700]!.withOpacity(0.5),
+                    body: Center(
+                      child: GestureDetector(
+                        // Do not clear modals if the modal itself is tapped
+                        onTap: () {},
+                        child: Container(
+                          color: Colors.white,
+                          child: state.last,
+                        ),
+                      ),
                     ),
                   ),
                 ),
