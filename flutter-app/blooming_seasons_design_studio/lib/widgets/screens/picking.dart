15d39d22ad848ebefd895ca1329766ce4f4f46f0
aatack <alexanderjtatack@gmail.com>
2023-05-23 21:35:25 +0100

    Move the loading indicator into a specialised folder

--- flutter-app/blooming_seasons_design_studio/lib/widgets/screens/picking.dart
+++ flutter-app/blooming_seasons_design_studio/lib/widgets/screens/picking.dart
@@ -2,7 +2,7 @@
 
 import 'dart:convert';
 
-import 'package:blooming_seasons_design_studio/widgets/loading.dart';
+import 'package:blooming_seasons_design_studio/widgets/indicators/loading.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:http/http.dart' as http;
