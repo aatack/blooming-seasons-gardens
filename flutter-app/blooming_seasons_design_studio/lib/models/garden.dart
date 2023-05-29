8de5ac0fbfa7cfc74c64c8c0ec6998ab6b2fabb2
aatack <alexanderjtatack@gmail.com>
2023-05-29 23:58:54 +0100

    Properly define the inheritance hierarchy of elements

--- flutter-app/blooming_seasons_design_studio/lib/models/garden.dart
+++ flutter-app/blooming_seasons_design_studio/lib/models/garden.dart
@@ -5,24 +5,24 @@ import 'package:flutter/material.dart' show Color, immutable;
 
 @immutable
 class Garden {
-  const Garden(this.name, this._beds, this._templates, this.availableID);
-
-  factory Garden.blank(String name) {
-    return Garden(name, const [], const {}, 1);
-  }
-
   final String name;
 
   final List<Instance<Bed>> _beds;
   UnmodifiableListView<Instance<Bed>> get beds => UnmodifiableListView(_beds);
 
   // Maps identifiers to a template associated with that identifier
-  final Map<int, Template> _templates;
-  UnmodifiableMapView<int, Template> get nursery =>
+  final Map<int, Element> _templates;
+  UnmodifiableMapView<int, Element> get nursery =>
       UnmodifiableMapView(_templates);
 
   // The next available identifier for elements in the garden
   final int availableID;
+
+  const Garden(this.name, this._beds, this._templates, this.availableID);
+
+  factory Garden.blank(String name) {
+    return Garden(name, const [], const {}, 1);
+  }
 
   /// Return a JSON-compatible representation of the garden.
   dynamic serialise() {
@@ -38,43 +38,32 @@ class Garden {
   }
 }
 
-@immutable
-class Template<Element> {
-  const Template(this.id, this.element);
+abstract class Element {}
 
-  final int id;
-  final Element element;
-}
+abstract class BedElement implements Element {}
 
 @immutable
-class Instance<Element> {
-  const Instance(this.id, this.x, this.y, this.element);
-
-  Instance.blank(Garden garden, Element newElement)
-      : id = garden.availableID,
-        x = 0.0,
-        y = 0.0,
-        element = newElement;
-
+class Instance<E extends Element> {
   final int id;
   final double x;
   final double y;
-  final Element element;
+  final E element;
+  final int? template;
+
+  const Instance(this.id, this.x, this.y, this.element, this.template);
 }
 
 @immutable
-class Bed {
-  const Bed(this._elements);
-
-  final List<Instance> _elements;
-  UnmodifiableListView<Instance> get elements =>
+class Bed implements Element {
+  final List<Instance<BedElement>> _elements;
+  UnmodifiableListView<Instance<BedElement>> get elements =>
       UnmodifiableListView(_elements);
+
+  const Bed(this._elements);
 }
 
 @immutable
-class Plant {
-  const Plant(this.name, this.size, this.plantType, this.border, this.image);
-
+class Plant implements BedElement {
   final String name;
 
   final double size;
@@ -82,40 +71,42 @@ class Plant {
   final PlantType plantType;
   final PlantBorder? border;
   final PlantImage? image;
+
+  const Plant(this.name, this.size, this.plantType, this.border, this.image);
 }
 
 enum PlantType { border, image }
 
 @immutable
 class PlantBorder {
-  const PlantBorder(this.thickness, this.colour);
-
   final double thickness;
   final Color colour;
+
+  const PlantBorder(this.thickness, this.colour);
 }
 
 @immutable
 class PlantImage {
-  const PlantImage(this.image, this.x, this.y, this.scale);
-
   final Image image;
   final double x;
   final double y;
   final double scale;
+
+  const PlantImage(this.image, this.x, this.y, this.scale);
 }
 
 @immutable
-class Label {
-  const Label(this.text, this.size);
-
+class Label implements BedElement {
   final String text;
   final double size;
+
+  const Label(this.text, this.size);
 }
 
 @immutable
-class Arrow {
-  const Arrow(this.x, this.y);
-
+class Arrow implements BedElement {
   final double x;
   final double y;
+
+  const Arrow(this.x, this.y);
 }
