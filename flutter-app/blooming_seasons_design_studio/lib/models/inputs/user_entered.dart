813c5f1884822eb36135bc39f94cf447d2d73f6b
aatack <alexanderjtatack@gmail.com>
2023-06-10 00:02:23 +0100

    Implement a user-entered double type

--- flutter-app/blooming_seasons_design_studio/lib/models/inputs/user_entered.dart
+++ flutter-app/blooming_seasons_design_studio/lib/models/inputs/user_entered.dart
@@ -9,3 +9,39 @@ abstract class UserEntered<Type> {
   bool get valid;
   Type get value;
 }
+
+class UserEnteredDouble extends UserEntered<double> {
+  final double? minimum;
+  final double? maximum;
+
+  const UserEnteredDouble(super.string, {this.minimum, this.maximum});
+
+  @override
+  bool get valid => _valid();
+
+  bool _valid() {
+    final parsedValue = double.tryParse(string);
+    return parsedValue != null &&
+        (minimum == null || parsedValue >= minimum!) &&
+        (maximum == null || parsedValue <= maximum!);
+  }
+
+  @override
+  double get value => double.parse(string);
+
+  Map<String, dynamic> serialise() {
+    return {
+      "string": string,
+      "minimum": minimum,
+      "maximum": maximum,
+    };
+  }
+
+  UserEnteredDouble deserialise(Map<String, dynamic> userEnteredDouble) {
+    return UserEnteredDouble(
+      userEnteredDouble["string"],
+      minimum: userEnteredDouble["minimum"],
+      maximum: userEnteredDouble["maximum"],
+    );
+  }
+}
