bfeea2d52165cc9c2882cea4ad119aed62c1285a
aatack <alexanderjtatack@gmail.com>
2023-06-12 01:32:01 +0100

    Set up a naive colour scheme

---
+++ flutter-app/blooming_seasons_design_studio/lib/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Theme {
+  static const Color backgroundColour = Color.fromARGB(1, 201, 248, 228);
+  static const Color neutralColour = Color.fromARGB(1, 139, 206, 185);
+  static const Color foregroundColour = Color.fromARGB(1, 78, 166, 153);
+
+  static const Color emphasiseColour = Color.fromARGB(1, 20, 13, 79);
+  static const Color textColour = Color.fromARGB(1, 28, 11, 25);
+}
