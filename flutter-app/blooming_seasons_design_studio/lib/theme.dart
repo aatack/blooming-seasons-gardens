738ecbbc30535a719d2732cdff187cdcb766cc69
aatack <alexanderjtatack@gmail.com>
2023-06-12 02:08:51 +0100

    Work out how to set button background colours

--- flutter-app/blooming_seasons_design_studio/lib/theme.dart
+++ flutter-app/blooming_seasons_design_studio/lib/theme.dart
@@ -1,12 +1,43 @@
-import 'package:flutter/material.dart';
+import 'dart:ui';
 
-class Theme {
-  static const Color backgroundColour = Color.fromARGB(1, 201, 248, 228);
-  static const Color neutralColour = Color.fromARGB(1, 139, 206, 185);
-  static const Color foregroundColour = Color.fromARGB(1, 78, 166, 153);
+import 'package:flutter/material.dart' hide Color;
 
-  static const Color emphasiseColour = Color.fromARGB(1, 20, 13, 79);
+class AppTheme {
+  static MaterialColor backgroundColour =
+      _createMaterialColour(const Color.fromARGB(255, 201, 248, 228));
+  static MaterialColor neutralColour =
+      _createMaterialColour(const Color.fromARGB(255, 139, 206, 185));
+  static MaterialColor foregroundColour =
+      _createMaterialColour(const Color.fromARGB(255, 78, 166, 153));
 
-  static const Color darkColour = Color.fromARGB(1, 28, 11, 25);
-  static const Color lightColour = Color.fromARGB(1, 247, 247, 242);
+  static MaterialColor emphasiseColour =
+      _createMaterialColour(const Color.fromARGB(255, 20, 13, 79));
+
+  static MaterialColor darkColour =
+      _createMaterialColour(const Color.fromARGB(255, 28, 11, 25));
+  static MaterialColor lightColour =
+      _createMaterialColour(const Color.fromARGB(255, 247, 247, 242));
+}
+
+MaterialColor _createMaterialColour(Color colour) {
+  List strengths = <double>[.05];
+
+  final swatch = <int, Color>{};
+  final int red = colour.red, green = colour.green, blue = colour.blue;
+
+  for (int i = 1; i < 10; i++) {
+    strengths.add(0.1 * i);
+  }
+  strengths.forEach((strength) {
+    final double delta = 0.5 - strength;
+
+    swatch[(strength * 1000).round()] = Color.fromRGBO(
+      red + ((delta < 0 ? red : (255 - red)) * delta).round(),
+      green + ((delta < 0 ? green : (255 - green)) * delta).round(),
+      blue + ((delta < 0 ? blue : (255 - blue)) * delta).round(),
+      1,
+    );
+  });
+
+  return MaterialColor(colour.value, swatch);
 }
--- flutter-app/blooming_seasons_design_studio/lib/widgets/inputs/button.dart
+++ flutter-app/blooming_seasons_design_studio/lib/widgets/inputs/button.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../theme.dart';
+
 class Button extends StatelessWidget {
   final void Function()? onClicked;
   final Widget child;
@@ -14,6 +16,15 @@ class Button extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(onPressed: onClicked, child: child);
+    final colour =
+        emphasise ? AppTheme.emphasiseColour : AppTheme.foregroundColour;
+
+    return TextButton(
+      onPressed: onClicked,
+      style: TextButton.styleFrom(
+          backgroundColor: colour,
+          shape: const RoundedRectangleBorder(borderRadius: BorderRadius.zero)),
+      child: child,
+    );
   }
 }
