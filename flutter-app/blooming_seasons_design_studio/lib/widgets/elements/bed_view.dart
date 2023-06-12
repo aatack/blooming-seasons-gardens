e3251fa1fa0c8605a76ad221c0d75777fc58cf8b
aatack <alexanderjtatack@gmail.com>
2023-06-12 01:03:34 +0100

    Check that disabling editing works

--- flutter-app/blooming_seasons_design_studio/lib/widgets/elements/bed_view.dart
+++ flutter-app/blooming_seasons_design_studio/lib/widgets/elements/bed_view.dart
@@ -16,30 +16,91 @@ class BedView extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.only(bottom: 8.0),
       child: Collapsible(
-        header: Text(bed.name),
-        child: Row(
-          children: [
-            ControlledTextInput(
-              value: bed.name,
-              onChange: (newValue, transient) {
-                context.read<SessionState>().editGarden(
-                    (garden) =>
-                        garden.editBed(bed.id, (bed) => bed.rename(newValue)),
-                    transient: transient);
-              },
-            ),
-            ControlledTextInput(
-              value: bed.name,
-              onChange: (newValue, transient) {
-                context.read<SessionState>().editGarden(
-                    (garden) =>
-                        garden.editBed(bed.id, (bed) => bed.rename(newValue)),
-                    transient: transient);
-              },
-            )
-          ],
-        ),
+        header: BedViewHeader(bed: bed),
+        child: BedViewContent(bed: bed),
       ),
+    );
+  }
+}
+
+class BedViewHeader extends StatefulWidget {
+  const BedViewHeader({
+    super.key,
+    required this.bed,
+  });
+
+  final Bed bed;
+
+  @override
+  State<BedViewHeader> createState() => _BedViewHeaderState();
+}
+
+class _BedViewHeaderState extends State<BedViewHeader> {
+  bool _editingName = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        ControlledTextInput(
+          value: widget.bed.name,
+          onChange: (newValue, transient) {
+            context.read<SessionState>().editGarden(
+                (garden) => garden.editBed(
+                    widget.bed.id, (bed) => bed.rename(newValue)),
+                transient: transient);
+          },
+          editing: _editingName,
+          onEditingFinished: () {
+            setState(() {
+              _editingName = false;
+            });
+          },
+        ),
+        if (!_editingName)
+          ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  _editingName = true;
+                });
+              },
+              child: const Text("Edit"))
+      ],
+    );
+  }
+}
+
+class BedViewContent extends StatelessWidget {
+  const BedViewContent({
+    super.key,
+    required this.bed,
+  });
+
+  final Bed bed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        ControlledTextInput(
+          value: bed.name,
+          onChange: (newValue, transient) {
+            context.read<SessionState>().editGarden(
+                (garden) =>
+                    garden.editBed(bed.id, (bed) => bed.rename(newValue)),
+                transient: transient);
+          },
+        ),
+        ControlledTextInput(
+          value: bed.name,
+          onChange: (newValue, transient) {
+            context.read<SessionState>().editGarden(
+                (garden) =>
+                    garden.editBed(bed.id, (bed) => bed.rename(newValue)),
+                transient: transient);
+          },
+        )
+      ],
     );
   }
 }
