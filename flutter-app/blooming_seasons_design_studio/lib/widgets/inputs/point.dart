8d4e70817e9e9df3a1b926fe156a2907a126d0a1
aatack <alexanderjtatack@gmail.com>
2023-06-18 21:39:05 +0100

    Provide proper feedback on input validation failures

--- flutter-app/blooming_seasons_design_studio/lib/widgets/inputs/point.dart
+++ flutter-app/blooming_seasons_design_studio/lib/widgets/inputs/point.dart
@@ -12,24 +12,32 @@ class PointInput extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
       children: [
-        const Text("Horizontal"),
-        const SizedBox(width: 5),
-        _wrapTextInput(
-          point.x,
-          (newValue, transient) {
-            onChange(Point(point.x.set(newValue), point.y), transient);
-          },
+        Row(
+          children: [
+            const Text("Horizontal"),
+            const SizedBox(width: 5),
+            _wrapTextInput(
+              point.x,
+              (newValue, transient) {
+                onChange(Point(point.x.set(newValue), point.y), transient);
+              },
+            ),
+          ],
         ),
-        const SizedBox(width: 20),
-        const Text("Vertical"),
-        const SizedBox(width: 5),
-        _wrapTextInput(
-          point.y,
-          (newValue, transient) {
-            onChange(Point(point.x, point.y.set(newValue)), transient);
-          },
+        const SizedBox(height: 8),
+        Row(
+          children: [
+            const Text("Vertical"),
+            const SizedBox(width: 5),
+            _wrapTextInput(
+              point.y,
+              (newValue, transient) {
+                onChange(Point(point.x, point.y.set(newValue)), transient);
+              },
+            ),
+          ],
         ),
       ],
     );
@@ -37,19 +45,38 @@ class PointInput extends StatelessWidget {
 
   Widget _wrapTextInput(
       ValidatedDouble value, void Function(String, bool) onInputChange) {
-    return Tooltip(
-      message: value.errors.isEmpty ? "" : value.errors.join("\n"),
-      excludeFromSemantics: true, // Prevents tooltip from grabbing focus
-      child: Container(
-        color: value.errors.isEmpty ? Colors.white : Colors.red[100],
-        child: Padding(
-          padding: const EdgeInsets.only(left: 5, right: 3),
-          child: ControlledTextInput(
-            value: value.string,
-            onChange: onInputChange,
-          ),
+    final content = Container(
+      color: value.errors.isEmpty ? Colors.white : Colors.red[100],
+      child: Padding(
+        padding: const EdgeInsets.only(left: 5, right: 3),
+        child: ControlledTextInput(
+          value: value.string,
+          onChange: onInputChange,
         ),
       ),
+    );
+
+    return Row(
+      children: [
+        content,
+        Visibility(
+          visible: value.errors.isNotEmpty,
+          maintainSize: true,
+          maintainAnimation: true,
+          maintainState: true,
+          /* For some reason, simply using a tooltip here causes the
+            tooltip to grab focus every time the message goes from empty
+            to non-empty.  This solution doesn't grab focus, and hence
+            doesn't interfere with the text input. */
+          child: Padding(
+            padding: const EdgeInsets.only(left: 5),
+            child: Tooltip(
+              message: value.errors.join("\n"),
+              child: const Icon(Icons.error_outline),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
