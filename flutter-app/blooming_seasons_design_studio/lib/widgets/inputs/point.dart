862bb6882e7f5d6218e08822361bde9941e2b2b4
aatack <alexanderjtatack@gmail.com>
2023-06-27 00:10:07 +0100

    Revert the changes to point inputs

--- flutter-app/blooming_seasons_design_studio/lib/widgets/inputs/point.dart
+++ flutter-app/blooming_seasons_design_studio/lib/widgets/inputs/point.dart
@@ -14,68 +14,63 @@ class PointInput extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FormLayout(
-      children: pointInputLayoutItems(point, onChange),
+      children: [
+        FormLayoutItem(
+          label: const Text("Horizontal"),
+          child: _wrapTextInput(
+            point.x,
+            (newValue, transient) {
+              onChange(Point(point.x.set(newValue), point.y), transient);
+            },
+          ),
+        ),
+        FormLayoutItem(
+          label: const Text("Vertical"),
+          child: _wrapTextInput(
+            point.y,
+            (newValue, transient) {
+              onChange(Point(point.x, point.y.set(newValue)), transient);
+            },
+          ),
+        )
+      ],
     );
   }
-}
 
-List<FormLayoutItem> pointInputLayoutItems(
-    Point point, void Function(Point, bool) setPoint) {
-  return [
-    FormLayoutItem(
-      label: const Text("Horizontal"),
-      child: _wrapTextInput(
-        point.x,
-        (newValue, transient) {
-          setPoint(Point(point.x.set(newValue), point.y), transient);
-        },
+  Widget _wrapTextInput(
+      ValidatedDouble value, void Function(String, bool) onInputChange) {
+    final content = Container(
+      color: value.errors.isEmpty ? Colors.white : Colors.red[100],
+      child: Padding(
+        padding: const EdgeInsets.only(left: 5, right: 3),
+        child: ControlledTextInput(
+          value: value.string,
+          onChange: onInputChange,
+        ),
       ),
-    ),
-    FormLayoutItem(
-      label: const Text("Vertical"),
-      child: _wrapTextInput(
-        point.y,
-        (newValue, transient) {
-          setPoint(Point(point.x, point.y.set(newValue)), transient);
-        },
-      ),
-    )
-  ];
-}
+    );
 
-Widget _wrapTextInput(
-    ValidatedDouble value, void Function(String, bool) onInputChange) {
-  final content = Container(
-    color: value.errors.isEmpty ? Colors.white : Colors.red[100],
-    child: Padding(
-      padding: const EdgeInsets.only(left: 5, right: 3),
-      child: ControlledTextInput(
-        value: value.string,
-        onChange: onInputChange,
-      ),
-    ),
-  );
-
-  return Row(
-    children: [
-      content,
-      Visibility(
-        visible: value.errors.isNotEmpty,
-        maintainSize: true,
-        maintainAnimation: true,
-        maintainState: true,
-        /* For some reason, simply using a tooltip here causes the
+    return Row(
+      children: [
+        content,
+        Visibility(
+          visible: value.errors.isNotEmpty,
+          maintainSize: true,
+          maintainAnimation: true,
+          maintainState: true,
+          /* For some reason, simply using a tooltip here causes the
             tooltip to grab focus every time the message goes from empty
             to non-empty.  This solution doesn't grab focus, and hence
             doesn't interfere with the text input. */
-        child: Padding(
-          padding: const EdgeInsets.only(left: 5),
-          child: Tooltip(
-            message: value.errors.join("\n"),
-            child: const Icon(Icons.error_outline),
+          child: Padding(
+            padding: const EdgeInsets.only(left: 5),
+            child: Tooltip(
+              message: value.errors.join("\n"),
+              child: const Icon(Icons.error_outline),
+            ),
           ),
-        ),
-      )
-    ],
-  );
+        )
+      ],
+    );
+  }
 }
