d4ce99cbb6d5fcfb8340b186921a6572c6c6e93f
aatack <alexanderjtatack@gmail.com>
2023-07-01 15:15:49 +0100

    Capture background clicks

--- flutter-app/blooming_seasons_design_studio/lib/widgets/screens/garden_view/planner.dart
+++ flutter-app/blooming_seasons_design_studio/lib/widgets/screens/garden_view/planner.dart
@@ -7,24 +7,34 @@ class Planner extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FractionallySizedBox(
-      widthFactor: 1,
-      heightFactor: 1,
-      child: Stack(
-        children: [
-          Positioned(
-            left: 500,
-            top: 100,
-            child: SizedBox(
-              width: 100,
-              height: 100,
-              child: Container(
-                decoration:
-                    BoxDecoration(shape: BoxShape.circle, color: Colors.red),
+    return GestureDetector(
+      onTap: () {
+        print("Clicked");
+      },
+      child: FractionallySizedBox(
+        widthFactor: 1,
+        heightFactor: 1,
+        child: Stack(
+          children: [
+            GestureDetector(
+                onTap: () {
+                  print("Clicked background");
+                },
+                child: Container(color: Colors.white)),
+            Positioned(
+              left: 500,
+              top: 100,
+              child: SizedBox(
+                width: 100,
+                height: 100,
+                child: Container(
+                  decoration:
+                      BoxDecoration(shape: BoxShape.circle, color: Colors.red),
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
